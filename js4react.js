5362ed6d61cfde1bb601d175c4858f529dd3d5fb
rasik <rasikkayasthav@gmail.com>
2025-11-29 02:36:06 +0545

    arrow func

---
+++ js4react.js
@@ -0,0 +1,72 @@
+
+function greetings(name) {
+    console.log(`Hello, ${name}!`);
+  }
+  
+greetings('John'); // Hello, John!
+//The arrow function syntax allows you to create a function expression that produces the same result as the code above.
+const greetings = name => {
+    console.log(`Hello, ${name}!`);
+  };
+  
+greetings('John'); // Hello, John!
+
+const myArray = [1, 2, 3];
+
+// From this:
+myArray.forEach(function (item) {
+  console.log(item);
+});
+
+// To this:
+myArray.forEach(item => console.log(item));
+//Arrow Functions Don’t Have this Binding
+const person = {
+    name: 'Nathan',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+  
+    showSkills() {
+      this.skills.forEach(function (skill) {
+        console.log(`${this.name} is skilled in ${skill}`);
+      });
+    },
+  };
+  
+  person.showSkills();
+  //undefined is skilled in HTML
+//undefined is skilled in CSS
+//undefined is skilled in JavaScript
+
+const person2 = {
+    name: 'Nathan',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+  
+    showSkills() {
+      this.skills.forEach(skill => {
+        console.log(`${this.name} is skilled in ${skill}`);
+      });
+    },
+  };
+  
+  person2.showSkills();
+  /*Nathan is skilled in HTML
+Nathan is skilled in CSS
+Nathan is skilled in JavaScript */
+
+//One of these situations is when you define an object method. Back to our person object example above, 
+//suppose you write the showSkills() method as an arrow function like this:
+const person3 = {
+    name: 'Nathan',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+  
+    showSkills: () => {
+      this.skills.forEach(skill => {
+        console.log(`${this.name} is skilled in ${skill}`);
+      });
+    },
+  };
+  
+  person3.showSkills();
+  //TypeError: Cannot read properties of undefined (reading 'forEach')
+  //When you declare an object method using the arrow function, the this keyword refers to the global object, 
+  //and the skills property is undefined there. Never use the arrow function when declaring a method.
